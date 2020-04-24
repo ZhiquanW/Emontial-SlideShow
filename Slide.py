43fe727d078a70113e1a35e5958179d28b40e93f
ZhiquanW <zhiquan.wzq@gmail.com>
2020-04-23 21:56:39 -0400

    tmp update

--- Slide.py
+++ Slide.py
@@ -12,10 +12,29 @@ import numpy as np
 
 
 class Slide(object):
-    def __init__(self, name: str, image: np.array, valence: float, arousal: float, intensity: float, dominant_color: np.array):
+    def __init__(self, id: int, name: str, image: np.array, valence: float, arousal: float, intensity: float, dominant_color: np.array):
+        self.id = id
         self.name = name
         self.image = image
         self.valence = valence
         self.arousal = arousal
         self.intensity = intensity
         self.dominant_color = dominant_color
+
+
+class TargetCurves(object):
+    def __init__(self, info_mat: List):
+        print(len(info_mat))
+        self.straight = info_mat[0]
+        self.straight_curve_down = info_mat[1]
+        self.straight_curve_hill = info_mat[2]
+        self.straight_curve_up = info_mat[3]
+        self.straight_u_shape = info_mat[4]
+        self.straight_down = info_mat[5]
+
+        self.straight_up = info_mat[6]
+
+# def cal_total_cost(slide_show:List,target_values:dict):
+#     valence_cost = 0
+#     value_mat = [[slide.valence, slide.arousal] for slide in slide_show]
+#     print(value_mat)
